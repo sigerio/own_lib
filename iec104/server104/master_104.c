b9ad2a8ae606faf329ac5547bd9d11b13fbb4ecd
sigerio <sigerio.joker@gmail.com>
2025-08-15 21:36:42 +0800

    if u do this again , u will hopelessgit add .!

---
+++ iec104/server104/master_104.c
@@ -0,0 +1,10 @@
+#include "pact104/iec_104.h"
+
+
+/*this file will manage the fsm of 104 server  , we called this 104A*/
+
+
+
+
+
+
---
+++ iec104/server104/master_104_define.h
@@ -0,0 +1,31 @@
+#ifndef __MASTER_104_DEFINE_H__
+#define __MASTER_104_DEFINE_H__
+
+
+
+
+
+enum
+{
+    MASTER_104_INIT = 0, //所谓初始化即主从站建立通讯连接的过程，该过程中必须完成U帧的STARTDT的下发与确认
+
+}MASTER_104_FSM;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
